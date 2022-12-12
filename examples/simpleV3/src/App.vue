b73468bab2aadf5de5f9e5506f74375a6c755165
mathieu dhainaut <mathieu.dhainaut@gmail.com>
2022-12-12 21:52:18 +0100

    Add simpleV3

---
+++ examples/simpleV3/src/App.vue
@@ -0,0 +1,191 @@
+<template>
+  <div id="app">
+    <dashboard :id="'dashExample'">
+      <dash-layout
+        v-for="layout in dlayouts"
+        v-bind="layout"
+        :debug="true"
+        :key="layout.breakpoint"
+      >
+        <dash-item
+          v-for="item in layout.items"
+          v-bind.sync="item"
+          :key="item.id"
+        >
+          <div class="content"></div>
+
+          <template v-slot:resizeBottomRight>
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 30 30"
+              focusable="false"
+              role="img"
+              alt="icon"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#42b983"
+              class="b-icon bi bi-arrow-down-right mx-auto"
+              data-v-11c9e491
+            >
+              <g data-v-11c9e491>
+                <path
+                  fill-rule="evenodd"
+                  d="M14 9.5a.5.5 0 01.5.5v5a.5.5 0 01-.5.5H9a.5.5 0 010-1h4.5V10a.5.5 0 01.5-.5z"
+                  clip-rule="evenodd"
+                ></path>
+                <path
+                  fill-rule="evenodd"
+                  d="M4.646 5.646a.5.5 0 01.708 0l9 9a.5.5 0 01-.708.708l-9-9a.5.5 0 010-.708z"
+                  clip-rule="evenodd"
+                ></path>
+              </g>
+            </svg>
+          </template>
+        </dash-item>
+      </dash-layout>
+    </dashboard>
+  </div>
+</template>
+
+<script>
+import { Dashboard, DashLayout, DashItem } from "vue-responsive-dash";
+
+export default {
+  name: "App",
+  components: {
+    Dashboard,
+    DashLayout,
+    DashItem,
+  },
+  data() {
+    return {
+      dlayouts: [
+        {
+          breakpoint: "xl",
+          numberOfCols: 12,
+          compact: true,
+          items: [
+            {
+              id: "1",
+              x: 0,
+              y: 0,
+              width: 1,
+              height: 1,
+            },
+            { id: "2", x: 1, y: 0, width: 2, height: 1 },
+            { id: "3", x: 0, y: 1, width: 2, height: 1 },
+            { id: "4", x: 3, y: 0, width: 2, height: 2 },
+            { id: "5", x: 5, y: 0, width: 1, height: 2 },
+            { id: "6", x: 6, y: 0, width: 2, height: 1 },
+            { id: "7", x: 7, y: 1, width: 5, height: 1 },
+          ],
+        },
+        {
+          breakpoint: "lg",
+          compact: true,
+          breakpointWidth: 1200,
+          numberOfCols: 10,
+          items: [
+            {
+              id: "1",
+              x: 0,
+              y: 0,
+              width: 1,
+              height: 1,
+            },
+            { id: "2", x: 1, y: 0, width: 2, height: 1 },
+            { id: "3", x: 0, y: 1, width: 2, height: 1 },
+            { id: "4", x: 3, y: 0, width: 2, height: 2 },
+            { id: "5", x: 5, y: 0, width: 1, height: 2 },
+            { id: "6", x: 6, y: 0, width: 2, height: 1 },
+            { id: "7", x: 7, y: 1, width: 3, height: 1 },
+          ],
+        },
+        {
+          breakpoint: "md",
+          breakpointWidth: 996,
+          compact: true,
+          numberOfCols: 8,
+          items: [
+            {
+              id: "1",
+              x: 0,
+              y: 0,
+              width: 1,
+              height: 1,
+            },
+            { id: "2", x: 1, y: 0, width: 2, height: 1 },
+            { id: "3", x: 0, y: 1, width: 2, height: 1 },
+            { id: "4", x: 3, y: 0, width: 2, height: 2 },
+            { id: "5", x: 5, y: 0, width: 1, height: 2 },
+            { id: "6", x: 6, y: 0, width: 2, height: 1 },
+            { id: "7", x: 7, y: 1, width: 1, height: 1 },
+          ],
+        },
+        {
+          breakpoint: "sm",
+          breakpointWidth: 768,
+          numberOfCols: 4,
+          compact: true,
+          items: [
+            {
+              id: "1",
+              x: 0,
+              y: 0,
+              width: 1,
+              height: 1,
+            },
+            { id: "2", x: 1, y: 0, width: 2, height: 1 },
+            { id: "3", x: 0, y: 1, width: 2, height: 1 },
+            { id: "4", x: 3, y: 0, width: 1, height: 2 },
+            { id: "5", x: 2, y: 1, width: 1, height: 1 },
+          ],
+        },
+        {
+          breakpoint: "xs",
+          breakpointWidth: 480,
+          numberOfCols: 2,
+          compact: true,
+          items: [
+            {
+              id: "1",
+              x: 0,
+              y: 0,
+              width: 1,
+              height: 1,
+            },
+            { id: "2", x: 1, y: 0, width: 1, height: 1 },
+            { id: "3", x: 0, y: 1, width: 2, height: 1 },
+          ],
+        },
+        {
+          breakpoint: "xxs",
+          breakpointWidth: 0,
+          compact: true,
+          numberOfCols: 1,
+          items: [
+            {
+              id: "1",
+              x: 0,
+              y: 0,
+              width: 1,
+              height: 1,
+            },
+            { id: "2", x: 0, y: 1, width: 1, height: 1 },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.content {
+  height: 100%;
+  width: 100%;
+  border: 2px solid #42b983;
+  border-radius: 5px;
+  background-color: #42b9833d;
+}
+</style>
